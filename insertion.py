3bcac76793e4cbe533434100fe2111e338d40b12
shailendra sharma <shaarma.shailendra813@gmail.com>
2025-04-07 12:35:47 +0530

    sorting algorithm

---
+++ insertion.py
@@ -0,0 +1,9 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j-1]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+            j -= 1
+    return arr      
+
+print(insertion_sort([64, 34, 25, 12, 22, 11, 90])) 
---
+++ sorted.py
@@ -0,0 +1,7 @@
+arr = [5, 2, 9, 1, 5, 6]
+arr.sort()  # Sorts in-place
+print(arr)
+
+# OR return a new sorted list
+sorted_arr = sorted(arr)
+print(sorted_arr)
